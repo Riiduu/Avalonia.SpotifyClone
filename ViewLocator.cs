6e9b17593947678edcc3ba100366d98d26324e4e
riiduu <jelinsksk@gmail.com>
2022-09-04 21:16:25 +0300

    Created the bare bones

--- ViewLocator.cs
+++ ViewLocator.cs
@@ -16,7 +16,7 @@ namespace Avalonia.SpotifyClone
             {
                 return (Control)Activator.CreateInstance(type)!;
             }
-            
+
             return new TextBlock { Text = "Not Found: " + name };
         }
 
