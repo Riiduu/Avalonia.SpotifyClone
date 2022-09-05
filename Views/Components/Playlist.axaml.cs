8df837e98af161de8edd6b6a1215327993980a30
riiduu <jelinsksk@gmail.com>
2022-09-05 12:22:59 +0300

    Created a mock playlist, which is still being worked out

---
+++ Views/Components/Playlist.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.SpotifyClone.Views.Components;
+
+public partial class Playlist : UserControl
+{
+    public Playlist()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public void ChangeImage(Avalonia.Media.IImage newImage)
+    {
+
+        PlaylistImage.Source = newImage;
+    }
+
+    public void ChangeName(string newText)
+    {
+        PlaylistName.Text = newText;
+    }
+}
---
+++ Views/Components/RecentPlaylists.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+
+namespace Avalonia.SpotifyClone.Views.Components;
+
+public partial class RecentPlaylists : UserControl
+{
+    public RecentPlaylists()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    void ChangeFirst()
+    {
+        Playlist1.ChangeImage(new Bitmap("../../Assets/playlists/Playlist-2.jpeg"));
+        Playlist1.ChangeName("Tavmamm");
+    }
+}
